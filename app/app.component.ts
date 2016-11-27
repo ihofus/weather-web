9ba520227926a2909658eac4541fabe9ad068403
Ovidijus <ovidijus.na@gmail.com>
2016-11-27 19:51:18 +0200

    use object notation

--- app/app.component.ts
+++ app/app.component.ts
@@ -10,10 +10,7 @@ import { Chanel } from './weather/chanel';
 export class AppComponent {
   public chanel1: Chanel;
   constructor() {
-    this.chanel1 = new Chanel();
-    this.chanel1.name = 'Test chanel';
-    this.chanel1.code = '194743';
-    this.chanel1.readApiKey = 'ISATC8TCNVTMVE80';
+    this.chanel1 = { name: 'Test place', code: '194743', readApiKey: 'ISATC8TCNVTMVE80' };
   }
 
 }
